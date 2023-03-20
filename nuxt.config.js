015c05b5d5f6aa82ff00c84a81e0b1fef91ba830
Tyner Roycroft <tynersemail@gmail.com>
2023-03-20 14:06:01 -0400

    Update Font

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "nuxt-blog",
+    title: "Collected Temporary Works.",
     htmlAttrs: {
       lang: "en",
     },
